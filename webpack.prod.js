efa8ddbd7a4ccda393b8ee8d9205aa6201423fba
Sergio <sergio.damian.silva@gmail.com>
2021-08-09 01:05:14 -0300

    primer commit

---
+++ webpack.prod.js
@@ -0,0 +1,76 @@
+const HtmlWebpack = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const Terser = require('terser-webpack-plugin');
+
+module.exports = {
+
+    mode: "production",
+
+    output: {
+        clean: true,
+        filename: 'main.[fullhash].js'
+    },
+
+    module: {
+        rules: [{
+                test: /\.html$/,
+                loader: 'html-loader',
+                options: {
+                    sources: false
+                }
+            },
+            {
+                test: /\.css$/,
+                exclude: /styles.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /styles.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            }
+
+        ]
+    },
+
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new Terser(),
+            new CssMinimizerPlugin()
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpack({
+            title: 'Mi Webpack App',
+            // filename: 'index.html',
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[fullhash].css',
+            ignoreOrder: false
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets/', to: 'assets/' }
+            ],
+        })
+    ]
+};
